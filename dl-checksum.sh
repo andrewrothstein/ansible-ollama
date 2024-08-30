32c53653f3ed00e452eddc68e19abbd24d9341f3
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-08-30 01:36:49 +0000

    ollama:0.3.8

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -10,14 +10,15 @@ dl()
     local lchecksums=$2
     local os=$3
     local arch=$4
+    local archive_type=${5:-tgz}
     local platform="${os}-${arch}"
-    local file="${APP}-${platform}"
+    local file="${APP}-${platform}.${archive_type}"
 
-    # https://github.com/ollama/ollama/releases/download/v0.1.41/ollama-linux-amd64
+    # https://github.com/ollama/ollama/releases/download/v0.3.8/ollama-linux-amd64.tgz
     local url="${MIRROR}/v${ver}/${file}"
 
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(egrep -e "$file\$" $lchecksums | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(egrep -e "$file" $lchecksums | awk '{print $1}')
 }
 
 dl_ver () {
@@ -36,4 +37,4 @@ dl_ver () {
     dl $ver $lchecksums linux arm64
 }
 
-dl_ver ${1:-0.3.6}
+dl_ver ${1:-0.3.8}
